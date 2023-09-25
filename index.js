34cec9c439067e6c9aab8ba0289fa6b9a1d58026
Daniel Long <danielwlong618@gmail.com>
2023-09-24 22:28:19 -0500

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData (userName, userEmail) {
+return fetch ('http://localhost:3000/users', {
+    method : 'POST',
+    headers : {
+        'content-type' : 'application/json',
+        'accept' : 'application/json'
+    },
+    body : JSON.stringify(dataObj)
+})
+
+.then ((resp) => resp.json())
+.then ((data) => {
+    let newId = data.id;
+    let idElement = document.createElement('div');
+    idElement.textContent = `New ID: ${newId}`;
+    document.body.appendChild(idElement)
+} )
+
+.catch(function (error) {
+    const errorMessage = document.createElement('div');
+    errorMessage.textContent = `Error: ${error.message}`;
+    document.body.appendChild(errorMessage);
+})
+}
+
+let dataObj = {
+    name : 'Steve',
+    email : 'steve@steve.com'
+}
